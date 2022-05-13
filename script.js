aca5ccef4e6725aab5823a3242ef54d5936f9e34
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-05-12 18:34:02 -0700

    After Bryan Helps me get direction
    class start 5/12/2022

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var minutes
 function Correct() {
     console.log("Correct");
 }
@@ -6,3 +7,19 @@ function Incorrect() {
     console.log("Incorrect");
 }
 
+//Change div that is displayed
+
+function QuestionSwap()
+
+//prevent defalt becuse of fomr make use of even listeners click event
+//attach event listner to html buttons
+//change buttons to radio buttons only for not submit
+//grab the value of of click events target
+//make a object with correct awnsers (array?)
+//logic to see if selected option is correct or incorrect
+//maybe add next button to add div swap logic
+//global scope of correct 
+// us primitive events In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods or properties. There are 7 primitive data types: string, number, bigint, boolean, undefined, symbol, and null. google
+
+function Timer()Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+console.log(timer);
