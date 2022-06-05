41ee8a12cb65f56c4a4b96fe779dd2266473bd38
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-06-05 13:57:10 -0700

    Score is working

--- script.js
+++ script.js
@@ -11,10 +11,11 @@ var awnser3 = document.getElementById('ButtonC')
 var awnser4 = document.getElementById('ButtonD')
 var Question = document.getElementById('Qdisplay')
 var selection = document.getElementsByName('selection')
+var score = 0
 // selection.addEventListener()
 
 function clickHandler(event) {
-  console.log('Button click',event);
+  console.log('Button click', event);
 }
 
 // const btn = document.querySelector('.btn');
@@ -42,9 +43,25 @@ function Incorrect() {
 
 document.getElementById('form').addEventListener("submit", function (event) {
   event.preventDefault()
-  let userSelection = event.target
-  console.log(userSelection.tagName)
-  console.log(userSelection.getAttribute("data-value"))
+  let userSelection = document.getElementsByName("selection")
+  userSelection.forEach(function (option) {
+    console.log(option.tagName)
+    console.log(option.getAttribute("data-value"), option.getAttribute("value"), option.checked)
+    if (option.checked) {
+      if (option.getAttribute("data-value") == questionsArray[CurrentQuestion].correctAnswer) {
+        score += 10;
+      }
+    } else {
+
+    }
+  })
+if(CurrentQuestion<questionsArray.length-1) {
+  CurrentQuestion++
+  pushQuestion()
+}
+else{
+console.log(score)
+}
 });
 
 
@@ -65,7 +82,7 @@ let questionsArray = [
   {
     questionText: "How would you use somthing from your html in JavaScript?",
     possibleAnswers: ["I have a chicken", "Class or ID", "3", "4"],
-    correctAnswer:1
+    correctAnswer: 1
   },
   {
     questionText: "What must end any string in javascript?",
@@ -75,33 +92,33 @@ let questionsArray = [
   {
     questionText: "What is the primart purpose of a function in Java Script?",
     possibleAnswers: ["To Create a complex or simple group of commands to achive an operation.", "2", "3", "4"],
-    correctAnswer: "To create a complex or simple group of commands to achive an operation."
+    correctAnswer: 0
   },
   {
     questionText: "Why do we need JavaScript at all and not just HTML or CSS?",
     possibleAnswers: ["JavaScript helps us do tasks that would be considered complex to achive with just html and css.", "2", "3", "4"],
-    correctAnswer: "JavaScript helps us do tasks that would be considered complex to achive with just html and css."
+    correctAnswer: 0
   },
   {
     questionText: "What is another name for an array in JavaScript?",
     possibleAnswers: ["1", "2", "A list.", "4"],
-    correctAnswer: "A list."
+    correctAnswer: 2
   },
   {
     questionText: "What is the basic purpose of creating an array?",
     possibleAnswers: ["1", "2", "3", "To make a group of items that the programmer can pull from."],
-    correctAnswer: "To make a group of items that the programmer can pull from."
+    correctAnswer: 3
   },
   {
     questionText: "What is the difference between Const and Var?",
     possibleAnswers: ["The scope of a var is functional scope while the scope of const is block scope.", "2", "3", "4"],
-    correctAnswer: "The scope of a var is functional scope while the scope of const is block scope."
+    correctAnswer: 0
   }
 ]
 
 
 
-subbmitQuiz.addEventListener('click',function(){
+subbmitQuiz.addEventListener('click', function () {
   Container.style.display = "block";
   subbmitQuiz.style.display = "none";
   pushQuestion();
