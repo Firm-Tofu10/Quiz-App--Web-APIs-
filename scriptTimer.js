d147979f858185c3ee09db2026e05c0bbaf26321
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-01 10:23:52 -0700

    cited source
    cited source/added a bit more to timer script

--- scriptTimer.js
+++ scriptTimer.js
@@ -3,18 +3,18 @@ countDownDate.setMinutes(countDownDate.getMinutes() + 10);
 var countDownTime = countDownDate.getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
-console.log("fdsfsf");
+
   // Get today's date and time
   var now = new Date().getTime();
 
   // Find the distance between now and the count down date
   var distance = countDownTime - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for minutes and seconds
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+  // Display the result in the element with id="timer"
   document.getElementById("timer").innerHTML = minutes + ":" + seconds;
 
   // If the count down is finished, write some text
