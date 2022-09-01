e29d8462b8755880a22d370e0db357dcadbac4ea
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-01 12:35:30 -0700

    Trying to get timer to start on start
    Trying to get timer to start on start

--- scriptTimer.js
+++ scriptTimer.js
@@ -1,32 +1,34 @@
 //https://www.w3schools.com/howto/howto_js_countdown.asp 
 //(This was starter code this was not an exact copy. It is modified to fit my needs as a count down timer and not a count down date.)
 var countDownDate = new Date();
-countDownDate.setMinutes(countDownDate.getMinutes() + 1);
+countDownDate.setMinutes(countDownDate.getMinutes() + 3);
 var countDownTime = countDownDate.getTime();
 // Update the count down every 1 second
-var x = setInterval(function() {
+if (document.getElementById("StartSubmit")) {
+	var x = setInterval(function () {
 
-  // Gets current time.
-  var now = new Date().getTime();
+		// Gets current time.
+		var now = new Date().getTime();
 
-  // Looks to see the timer length.
-  var distance = countDownTime - now;
+		// Looks to see the timer length.
+		var distance = countDownTime - now;
 
-  // Vars for minutes and seconds.
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		// Vars for minutes and seconds.
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in id="timer"
-  document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+		// Display the result in id="timer"
+		document.getElementById("timer").innerHTML = minutes + ":" + seconds;
 
-  // On timer end displays text. (Still working on a way on timer end to display score and end quizz.)
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "Times Up!";
-		article.style.display = "none";
-		NextButton.style.display = "none";
-		Qdisplay.style.display = "none";
-		finalScore.style.display = "flex";
-		document.getElementById("final").innerHTML = score;
-  }
-}, 1000);
+		// On timer end displays text. (Still working on a way on timer end to display score and end quizz.)
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("timer").innerHTML = "Times Up!";
+			article.style.display = "none";
+			NextButton.style.display = "none";
+			Qdisplay.style.display = "none";
+			finalScore.style.display = "flex";
+			document.getElementById("final").innerHTML = score;
+		}
+	}, 1000);
+}
